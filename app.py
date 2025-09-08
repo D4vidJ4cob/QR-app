b50eb6f7bcd1fc884866dca6e4c5ce83f2005a63
DAVID JACOB <david.jacob@student.hogent.be>
2025-09-09 00:45:21 +0200

    hide db password

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ from flask import Flask, render_template
 from extensions import db
 from flask_scss import Scss
 import models
+import os
+from dotenv import load_dotenv
+
+# Load dotenv
+load_dotenv()
 
 app = Flask(__name__)
 Scss(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://root:Tibenater15!@localhost/dnd_mushrooms"
+# Get password from .env
+password = os.getenv("DB_PASSWORD")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://root:{password}@localhost/dnd_mushrooms"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app) 
+
+
 
 @app.route("/")
 def index():
